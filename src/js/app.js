5e0d02aeb5bd12a9f0cb608dc9e2da805f8db620
eujinong <eujinong1@gmail.com>
2020-06-24 23:07:59 +0300

    contact page

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,17 @@ import '@fancyapps/fancybox';
 import $ from 'jquery';
 import 'slick-carousel';
 
+$('.carousel').slick({
+  infinite: false,
+  centerMode: true,
+  dots: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
